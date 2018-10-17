cb4e4433790e199e4831f189c0929097a0cfda4e
Taylor Kimmett <tkimmett@outlook.com>
2018-10-17 10:44:41 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h2>Top component sees: {{ $data }}</h2>
+    <hr />
+    <Test v-bind:field="$data" />
+  </div>
+</template>
+
+<script>
+import Test from "./components/Test";
+
+export default {
+  name: "App",
+  data: () => ({
+    x: 1,
+    y: "one",
+    z: "hello"
+  }),
+  components: {
+    Test
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <div v-for="(val, key) in field" :key="key">
+      <label>
+        {{ key }}
+        <input v-model="$props['field'][key]" @input="$emit('updated')" />
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    field: {
+      type: Object
+    }
+  },
+  methods: {
+    update: function(e) {
+      console.log("updated");
+      this.field.unknown = new Date().toString();
+      this.$emit("updated");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
